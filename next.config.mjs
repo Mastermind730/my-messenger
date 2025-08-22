c73d5b9da09a5a4849396b82c3d0bceeceafae7f
Sourav Narvekar <72343319+Mastermind730@users.noreply.github.com>
2025-08-22 15:57:04 +0530

    Update next.config.mjs for optimised build

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     assetPrefix: 'https://my-messenger-assets-bucket.s3.amazonaws.com/',
+      output: 'export',
+      trailingSlash: true,
     images: {
       domains: [
         "res.cloudinary.com",
         "avatars.githubusercontent.com",
         "lh3.googleusercontent.com",
         "my-messenger-assets.s3.amazonaws.com",
-      ]
+      ],
+            unoptimized: true,
+
     },
     reactStrictMode:true,
     // experimental: {
