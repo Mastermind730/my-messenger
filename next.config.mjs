ead90f670946ba3640bb7710a44faffc44074536
Sourav Narvekar <72343319+Mastermind730@users.noreply.github.com>
2025-08-22 15:54:16 +0530

    Update next.config.mjs for aws deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    assetPrefix: 'https://my-messenger-assets-bucket.s3.amazonaws.com/',
     images: {
       domains: [
         "res.cloudinary.com",
         "avatars.githubusercontent.com",
-        "lh3.googleusercontent.com"
+        "lh3.googleusercontent.com",
+        "my-messenger-assets.s3.amazonaws.com",
       ]
     },
+    reactStrictMode:true,
     // experimental: {
     //   swcPlugins: [
     //     ["next-superjson-plugin", {}]
